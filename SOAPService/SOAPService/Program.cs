252fbfbe5a520f7901011b446b25cc552e491fba
bruce8wang <bruce8wang@qq.com>
2018-02-10 17:13:36 +0800

    fifth commit

---
+++ SOAPService/SOAPService/Program.cs
@@ -0,0 +1,21 @@
+﻿using CustomMiddleware;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.ServiceModel;
+
+namespace SOAPService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => { services.AddScoped<CalculatorService>(); })
+                .Configure(app=> { app.UseSOAPMiddleware<CalculatorService>("/CalculatorService", new BasicHttpBinding()); })
+                .Build()
+                .Run();
+        }
+    }
+}

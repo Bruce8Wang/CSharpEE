6580c0a0754c7d01d7b8c5e015eb9cb7b52fb7e1
bruce8wang <bruce8wang@qq.com>
2018-02-10 17:08:21 +0800

    third commit

---
+++ NTLM/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace com.example.demo.Controllers
+{
+    public  class DefaultController:ApiController
+    {
+        [Route("api/Default")]
+        [HttpGet]
+        [Authorize]
+        public string Home()
+        {
+            return User.Identity.Name;
+        }
+    }
+}
---
+++ NTLM/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Net;
+using System.Web.Http;
+
+namespace com.example.demo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //设置使用Windows认证
+            HttpListener listener = (HttpListener)app.Properties["System.Net.HttpListener"];
+            listener.AuthenticationSchemes = AuthenticationSchemes.Ntlm;
+
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+
+            var opt = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                EnableDirectoryBrowsing = true,
+                FileSystem = new PhysicalFileSystem(string.Format("{0}/static", Environment.CurrentDirectory))
+            };
+            app.UseFileServer(opt);
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html";
+                return context.Response.WriteAsync("Hello World !");
+            });
+        }
+    }
+}

b97b7fac2fae105f5416e0a9d7369c69212876fc
bruce8wang <bruce8wang@qq.com>
2018-02-20 16:46:45 +0800

    commit ODN project

---
+++ ODN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ODN
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //定义一个Button
+            var btnSubmit = new Button();
+            btnSubmit.Click += (sender, e) => { MessageBox.Show("Hello World !"); };
+            //定义一个Form
+            var frm = new Form();
+            frm.Load += (sender, e) =>
+            {
+                btnSubmit.Location = new Point(120, 80);
+                btnSubmit.Size = new Size(75, 25);
+                btnSubmit.Text = "Submit";
+                frm.ClientSize = new Size(400, 300);
+                frm.Text = "主表单";
+                frm.Controls.Add(btnSubmit);
+            };
+            //运行一个Form
+            Application.Run(frm);
+        }
+    }
+}

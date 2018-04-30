f85b00ee3010d10b991c8693b3a94c72930006ea
bruce8wang <bruce8wang@qq.com>
2018-04-30 18:43:37 +0800

    导入WPF Demo应用

---
+++ Wpf-Demo/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Wpf_Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World !");
+        }
+    }
+}
---
+++ Wpf-Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace Wpf_Demo
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application app = new Application();
+            app.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+            app.Run();
+        }
+    }
+}

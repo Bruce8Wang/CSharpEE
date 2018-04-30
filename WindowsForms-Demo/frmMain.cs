5cfb3cf14311e4b7a97733a0603c1a510ac7eda6
bruce8wang <bruce8wang@qq.com>
2018-04-30 13:27:39 +0800

    Import Windows Form Project

---
+++ WindowsForms-Demo/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsForms_Demo
+{
+    public sealed class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            var btnSubmit = new Button
+            {
+                Location = new Point(250, 200),
+                Size = new Size(75, 25),
+                Name = "btnSubmit",
+                Text = "Submit"
+            };
+
+            Load += (sender, e) =>
+            {
+                ClientSize = new Size(800, 600);
+                Name = "frmMain";
+                Text = "frmMain";
+                Controls.Add(btnSubmit);
+            };
+
+            btnSubmit.Click += (sender, e) => 
+			{ 
+				MessageBox.Show("Hello World !"); 
+			};
+        }
+    }
+}

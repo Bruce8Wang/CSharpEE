a0625e5c7f199c154bc7c6650616ef718f14b9e3
Bruce <Bruce8Wang@qq.com>
2018-09-08 21:47:46 +0800

    代码整理

---
+++ WinForms-Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinForms_Demo
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
---
+++ WinForms-Demo/frmMain.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms_Demo
+{
+    public sealed class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            var btnSubmit = new Button
+            {
+                Location = new Point(250, 200),
+                Size = new Size(75, 25),
+                Text = "Submit"
+            };
+
+            Load += (sender, e) =>
+            {
+                ClientSize = new Size(800, 600);
+                Text = "frmMain";
+                Controls.Add(btnSubmit);
+            };
+
+            btnSubmit.Click += (sender, e) =>
+            {
+                MessageBox.Show("Hello World !");
+            };
+        }
+    }
+}

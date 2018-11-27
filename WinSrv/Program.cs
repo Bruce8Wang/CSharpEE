ccd2d907243b1f9840f0e4173758352d802c4c52
bruce8wang <bruce8wang@qq.com>
2018-11-27 11:15:10 +0800

    Import Windows Service Project .

---
+++ WinSrv/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace WinSrv
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ServiceBase.Run(new ServiceBase());
+		}
+		
+		public class WindowsService : ServiceBase
+		{
+			private Process process = null;
+			
+			protected override void OnStart(string[] args)
+			{
+				//执行逻辑或调用其他进程
+				process = Process.Start(ConfigurationManager.AppSettings["fileName"]);
+			}
+
+			protected override void OnStop()
+			{
+				//执行逻辑或杀死其他进程
+				process.Kill();
+			}
+		}
+	}
+	
+}

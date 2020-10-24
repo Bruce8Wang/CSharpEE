65fc6670e09ac5b1840aa6826a7adbb1954b2b42
Bruce8Wang <Bruce8Wang@qq.com>
2020-10-24 09:15:13 +0800

    Delete MySoapHeader.cs

--- WebServices-Demo/App_Code/MySoapHeader.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.Services.Protocols;
-
-public class MySoapHeader : SoapHeader
-{
-    public string UserName { get; set; }
-    public string Ip { get; set; }
-    public string Token { get; set; }
-}
